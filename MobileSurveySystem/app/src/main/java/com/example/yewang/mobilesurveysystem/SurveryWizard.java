ec881b3ff8d471c055d686ef66186035f76d2f1d
ferroye <yewang8864@gmail.com>
2018-11-08 17:15:17 -0500

    s

---
+++ MobileSurveySystem/app/src/main/java/com/example/yewang/mobilesurveysystem/SurveryWizard.java
@@ -0,0 +1,13 @@
+package com.example.yewang.mobilesurveysystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurveryWizard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survery_wizard);
+    }
+}
